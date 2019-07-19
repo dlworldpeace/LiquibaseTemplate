01f8d4ec46bb9ef272127568c8a7fc21a2030704
dlworldpeace <xdl_line@hotmail.com>
2019-07-19 13:51:46 +0800

    Fix #2 by ensuring latest changeset is updated upon build

--- src/test/java/com/sample/backend/DemoApplicationTests.java
+++ src/test/java/com/sample/backend/DemoApplicationTests.java
@@ -1,15 +1,5 @@
 package com.sample.backend;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import liquibase.Contexts;
-import liquibase.LabelExpression;
-import liquibase.Liquibase;
-import liquibase.database.Database;
-import liquibase.database.DatabaseFactory;
-import liquibase.database.jvm.JdbcConnection;
-import liquibase.exception.LiquibaseException;
-import liquibase.resource.ClassLoaderResourceAccessor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,17 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DemoApplicationTests {
 
     @Test
-    public void contextLoads()
-        throws LiquibaseException, SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.jdbc.Driver");
-        java.sql.Connection connection = DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/liquibasetest","root","mysql");
-        Database database = DatabaseFactory.getInstance()
-            .findCorrectDatabaseImplementation(new JdbcConnection(connection));
-        Liquibase liquibase = new liquibase.Liquibase(
-            "db/changelog/db.changelog-master.xml",
-            new ClassLoaderResourceAccessor(), database);
-        liquibase.update(new Contexts(), new LabelExpression());
+    public void contextLoads() {
+
     }
 
 }
